3925a070566bd643472d9c5392646b5d314aa811
sfmalloy <sfmalloy@millersville.edu>
2018-12-02 01:25:07 -0500

    Day02 added.

--- Advent.cc
+++ Advent.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <fstream>
 
-#include "Day01.hpp"
+#include "Day02.hpp"
 
 int main(int argc, char* argv[]) {
-    std::ifstream file1("Day01.txt");
-    std::ifstream file2("Day01.txt");
+    std::ifstream file1("Day02.txt");
+    std::ifstream file2("Day02.txt");
     solve(file1, std::cout);
     solve2(file2, std::cout);
 }
