770d5b24e434c003e8aaa0637aa3f247c15025e4
sfmalloy <sfmalloy@millersville.edu>
2018-12-05 15:08:21 -0500

    Day05 optimization (still not great but 1/3 of the time from before).

--- Day05.hpp
+++ Day05.hpp
@@ -10,9 +10,9 @@
 
 bool checkCase(const char& c1, const char& c2);
 
-size_t react(std::string& input);
+size_t react(std::string input);
 
-void reactSingle(std::string& input, const char& sub);
+std::string removeLetter(std::string input, const char& sub);
 
 template<>
 void Day<5>::solve1(std::ifstream& in, std::ostream& out) {
@@ -27,51 +27,37 @@ void Day<5>::solve2(std::ifstream& in, std::ostream& out) {
     std::string input;
     in >> input;
 
-    std::pair<char, size_t> maxLetter = { ' ', 0 };
+    size_t smallestSize = input.size();
     std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    
-    for (size_t i = 0; i < 26; ++i) {
-        size_t tempCount = 0;
 
-        for (size_t j = 0; j < input.size(); ++j) {
-            if (alphabet[i] == std::tolower(input[j]))
-                ++tempCount;
-        }
+    for (const char& l : alphabet) {
+        size_t currReact = react(removeLetter(input, l));
 
-        if (tempCount > maxLetter.second)
-            maxLetter = { alphabet[i], tempCount };
+        if (currReact < smallestSize)
+            smallestSize = currReact;
     }
 
-    reactSingle(input, maxLetter.first);
-    react(input);
-    out << input << std::endl;
+    out << smallestSize << std::endl;
 }
 
 bool checkCase(const char& c1, const char& c2) {
-    return (((std::islower(c1) && std::isupper(c2)) || (std::islower(c2) && std::isupper(c1))) && std::tolower(c1) == std::tolower(c2));
+    return (std::islower(c1) && std::isupper(c2)) || (std::islower(c2) && std::isupper(c1));
 }
 
-size_t react(std::string& input) {
+size_t react(std::string input) {
     bool hasRemoved = true;
     
     while (input.size() > 0 && hasRemoved) {
         hasRemoved = false;
-        std::vector<bool> destroyed(input.size(), false);
-
-        for (size_t i = 0; i < input.size() - 1; ++i) {
-            if (checkCase(input[i], input[i + 1])) {
-                destroyed[i] = true;
-                destroyed[i + 1] = true;
-                hasRemoved = true;
-                ++i;
-            }
-        }
 
         std::string tempInput;
-
         for (size_t i = 0; i < input.size(); ++i) {
-            if (!destroyed[i])
+            if (i < input.size() - 1 && std::tolower(input[i]) == std::tolower(input[i + 1]) && checkCase(input[i], input[i + 1])) {
+                hasRemoved = true;
+                ++i;
+            } else {
                 tempInput.push_back(input[i]);
+            }
         }
 
         input = tempInput;
@@ -80,14 +66,13 @@ size_t react(std::string& input) {
     return input.size();
 }
 
-void reactSingle(std::string& input, const char& sub) {
-    std::string tempInput;
-
+std::string removeLetter(std::string input, const char& sub) {
+    std::string newInput;
+    
     for (size_t i = 0; i < input.size(); ++i) {
-        if (input[i] != sub && input[i] != std::toupper(sub)) {
-            tempInput.push_back(input[i]);
-        }
+        if (input[i] != sub && input[i] != std::toupper(sub))
+            newInput.push_back(input[i]);
     }
 
-    input = tempInput;
+    return newInput;
 }
