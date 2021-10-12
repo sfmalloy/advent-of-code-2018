1701d70fffff5b9caf775c2cb6e77d7ca956b0b2
Sean Malloy <techfan321@gmail.com>
2021-10-12 16:38:07 -0400

    Day 4

--- solutions/d04.cpp
+++ solutions/d04.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <numeric>
+#include <tuple>
 
 #include "solution.hpp"
 #include "utils.hpp"
@@ -8,16 +10,21 @@
 class timestamp
 {
 public:
-    timestamp(u32 year, u32 month, u32 day, u32 hour, u32 minute, u32 id, bool is_awake)
+    timestamp(u32 year, u32 month, u32 day, u32 hour, u32 minute, u32 id, bool is_awake, const std::string& state)
         : m_year(year),
           m_month(month),
           m_day(day),
           m_hour(hour),
           m_minute(minute),
           m_id(id),
-          m_is_awake(is_awake)
+          m_is_awake(is_awake),
+          m_state(state)
     {
-        m_encoding = ((u64) m_year << 35) + ((u64) m_month << 21) + ((u64) m_day << 16) + ((u64) m_hour << 11) + ((u64) m_minute << 6);
+        m_encoding = ((u64) m_year << 35)
+            + ((u64) m_month << 21)
+            + ((u64) m_day << 16)
+            + ((u64) m_hour << 11)
+            + ((u64) m_minute << 6);
     }
 
     bool
@@ -35,12 +42,13 @@ public:
     std::string
     to_string() const
     {
-        return std::to_string(m_id) + " => "
-            + std::to_string(m_year) + "/"
-            + std::to_string(m_month) + "/"
-            + std::to_string(m_day) + " "
-            + std::to_string(m_hour) + ":"
-            + std::to_string(m_minute);
+        return std::to_string(m_id)    + " => "
+            + std::to_string(m_year)   + "/"
+            + std::to_string(m_month)  + "/"
+            + std::to_string(m_day)    + " "
+            + std::to_string(m_hour)   + ":"
+            + std::to_string(m_minute) + " => "
+            + m_state;
     }
 
     u32
@@ -82,7 +90,16 @@ private:
     u32 m_id;
     bool m_is_awake;
     u64 m_encoding;
+
+    std::string m_state;
 };
+
+std::ostream&
+operator<<(std::ostream& out, const timestamp& t)
+{
+    out << t.to_string();
+    return out;
+}
 
 template <>
 void
@@ -107,25 +124,48 @@ solution<4>::solve(std::ifstream& input)
             id = std::stoul(status.substr(num_idx, space_idx - num_idx));
         }
 
-        state_list.emplace_back(year, month, day, hour, minute, id, status[0] == 'w');
+        state_list.emplace_back(year, month, day, hour, minute, id, status[0] == 'w', status);
     }
 
     std::sort(std::begin(state_list), std::end(state_list));
-
-    std::unordered_map<u32, u32> sleep_times;
+    std::unordered_map<u32, std::array<u32, 60>> minutes_asleep;
 
     for (u32 i = 0; i < state_list.size(); ++i)
     {
         if (i > 0 && state_list[i].id() == 0)
-            state_list[i].set_id(state_list[i - 1].id());
-
-        if (!state_list[i].is_awake())
-            sleep_times[state_list[i].id()] += state_list[i].minute();
+        {
+            u32 id = state_list[i - 1].id();
+            state_list[i].set_id(id);
+            if (!state_list[i].is_awake())
+                for (u32 m = state_list[i].minute(); m < state_list[i + 1].minute(); ++m)
+                    ++minutes_asleep[id][m];
+        }
     }
 
-    u32 max = std::max_element(std::begin(sleep_times), std::end(sleep_times), [] (const auto& p1, const auto& p2) {
-        return p1.second < p2.second;
-    })->second;
+    std::pair<u32, u32> max_asleep = std::make_pair(0, 0);
+    for (const auto& [id, minutes] : minutes_asleep)
+    {
+        u32 total = std::accumulate(std::begin(minutes), std::end(minutes), 0);
 
-    std::cout << max << '\n';
+        if (total > max_asleep.second)
+            max_asleep = std::make_pair(id, total);
+    }
+
+    std::pair<u32, u32> max_minute = std::make_pair(0, 0);
+    for (u32 m = 0; m < 60; ++m)
+    {
+        u32 curr_minute_slept = minutes_asleep[max_asleep.first][m];
+        if (curr_minute_slept > max_minute.second)
+            max_minute = std::make_pair(m, curr_minute_slept);
+    }
+
+    std::cout << max_asleep.first * max_minute.first << '\n';
+
+    std::tuple<u32, u32, u32> frequent_minute = std::make_tuple<u32, u32, u32>(0, 0, 0);
+    for (const auto& [id, minutes] : minutes_asleep)
+        for (u32 m = 0; m < 60; ++m)
+            if (minutes[m] > std::get<2>(frequent_minute))
+                frequent_minute = std::make_tuple<u32, u32, u32>((u32) id, (u32) m, (u32) minutes[m]);
+
+    std::cout << std::get<0>(frequent_minute) * std::get<1>(frequent_minute) << '\n';
 }
