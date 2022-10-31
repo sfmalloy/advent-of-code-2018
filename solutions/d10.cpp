a02ef38cd7a4eed21f4c7ab0516a124b75024463
Sean Malloy <techfan321@gmail.com>
2022-10-31 19:58:07 -0400

    Started day 10

--- solutions/d10.cpp
+++ solutions/d10.cpp
@@ -2,9 +2,21 @@
 
 #include "solution.hpp"
 
+struct vec2
+{
+    i32 x;
+    i32 y;
+};
+
+struct star
+{
+    vec2 vel;
+    vec2 pos;
+};
+
 template <>
 void
 solution<10>::solve(std::ifstream& input)
 {
-    std::cout << "Solution to day 10" << '\n';
+    
 }
