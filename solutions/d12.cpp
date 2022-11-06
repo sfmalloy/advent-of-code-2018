7b012050c3f1a5f339b837e112ff35ddbfb1cae0
Sean Malloy <techfan321@gmail.com>
2022-11-05 21:11:27 -0400

    Day 12, start 13

--- solutions/d12.cpp
+++ solutions/d12.cpp
@@ -13,6 +13,7 @@ public:
       , positive(init)
       , negative_buffer("....")
       , positive_buffer(init)
+      , m_state_num(0)
     { }
 
     char&
@@ -43,9 +44,6 @@ public:
     i64
     start()
     {
-        // while (negative.size() > 0 && negative.substr(0, 4) != "....") {
-        //     negative = negative.substr(1);
-        // }
         while (positive.substr(positive.size() - 4) != "....") {
             positive += '.';
         }
@@ -69,11 +67,32 @@ public:
             negative_buffer[negative_buffer.size() + idx];
     }
 
+    bool
+    stabalized()
+    {
+        auto before = positive.substr(positive.find('#'), positive.rfind('#') - positive.find('#'));
+        auto after = positive_buffer.substr(positive_buffer.find('#'), positive_buffer.rfind('#') - positive_buffer.find('#'));
+        return before == after;
+    }
+
     void
     flush()
     {
         negative = negative_buffer;
         positive = positive_buffer;
+        ++m_state_num;
+    }
+
+    i64
+    state_num()
+    {
+        return m_state_num;
+    }
+
+    void
+    set_state_num(i64 new_state)
+    {
+        m_state_num = new_state;
     }
 
 private:
@@ -82,6 +101,8 @@ private:
 
     std::string negative_buffer;
     std::string positive_buffer;
+
+    i64 m_state_num;
 };
 
 template<>
@@ -98,9 +119,8 @@ solution<12>::solve(std::ifstream& input)
         rules[rule] = result[0];
     }
 
-
     state s{init_state};
-    for (u64 i = 0; i < 20; ++i)
+    while (s.state_num() < 20)
     {
         for (auto idx = s.start(); idx < s.last() - 4; ++idx)
         {
@@ -119,4 +139,26 @@ solution<12>::solve(std::ifstream& input)
         sum += s[i] == '#' ? i : 0;
     }
     std::cout << sum << '\n';
+
+    s.set_state_num(s.state_num() - 1);
+    do
+    {
+        s.flush();
+        for (auto idx = s.start(); idx < s.last() - 4; ++idx)
+        {
+            auto curr = s.substate(idx);
+            if (rules.contains(curr))
+                s[idx + 2] = rules[curr];
+            else
+                s[idx + 2] = '.';
+        }
+    } while (!s.stabalized());
+
+    constexpr const i64 fifty_billion = 50000000000;
+    i64 big_sum = 0;
+    for (i32 i = s.start(); i < s.last(); ++i)
+    {
+        big_sum += (s[i] == '#' ? ((i - s.state_num()) + fifty_billion) : 0);
+    }
+    std::cout << big_sum << '\n';
 }
--- solutions/d13.cpp
+++ solutions/d13.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "solution.hpp"
 
-template <>
+enum direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+};
+
+struct cart {
+    u32 x;
+    u32 y;
+    direction dir;
+    u32 irule;
+};
+
+template<>
 void
 solution<13>::solve(std::ifstream& input)
 {
-    std::cout << "Solution to day 13" << '\n';
+    std::vector<std::string> map(1);
+    while (std::getline(input, map.back()))
+        map.push_back("");
+    map.pop_back();
 }
+
+/**
+intersections:
+    1. go left
+    2. go straight
+    3. go right
+    4. repeat from 1
+*/
