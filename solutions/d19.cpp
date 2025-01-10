0b9167e01d2076246a3f362c06be9d6c67c7b655
Sean Malloy <techfan321@gmail.com>
2025-01-10 17:18:13 -0500

    day 21

--- solutions/d19.cpp
+++ solutions/d19.cpp
@@ -6,34 +6,11 @@
 #include "solution.hpp"
 #include "utils.hpp"
 
-// global d19_registers for convenience
-i32 registers[6]{0, 0, 0, 0, 0};
-
-// instruction declarations
-inline void addr(i32 a, i32 b, i32 c);
-inline void addi(i32 a, i32 b, i32 c);
-
-inline void mulr(i32 a, i32 b, i32 c);
-inline void muli(i32 a, i32 b, i32 c);
-
-inline void banr(i32 a, i32 b, i32 c);
-inline void bani(i32 a, i32 b, i32 c);
-
-inline void borr(i32 a, i32 b, i32 c);
-inline void bori(i32 a, i32 b, i32 c);
-
-inline void setr(i32 a, i32 b, i32 c);
-inline void seti(i32 a, i32 b, i32 c);
-
-inline void gtir(i32 a, i32 b, i32 c);
-inline void gtri(i32 a, i32 b, i32 c);
-inline void gtrr(i32 a, i32 b, i32 c);
-
-inline void eqir(i32 a, i32 b, i32 c);
-inline void eqri(i32 a, i32 b, i32 c);
-inline void eqrr(i32 a, i32 b, i32 c);
-
 using itype = void(*)(i32, i32, i32);
+namespace d19
+{
+
+i32 registers[6]{0, 0, 0, 0, 0};
 
 std::unordered_map<std::string, itype> instruction_fn{
     { "addr", [](i32 a, i32 b, i32 c) { registers[c] = registers[a] + registers[b]; } },
@@ -61,8 +38,10 @@ struct ProgramLine {
     i32 c;
 };
 
-void run(i32* ip, const std::vector<ProgramLine>& prog);
-u32 factor_sum(bool part2, i32* ip, const std::vector<ProgramLine>& prog);
+}
+
+void run(i32* ip, const std::vector<d19::ProgramLine>& prog);
+u32 factor_sum(bool part2, i32* ip, const std::vector<d19::ProgramLine>& prog);
 
 template<>
 void
@@ -72,23 +51,23 @@ solution<19>::solve(std::ifstream& input)
 
     std::getline(input, line);
     auto [_, ip_s] = fixed_split<2>(line);
-    i32* ip = &registers[convert<u32>(ip_s)];
+    i32* ip = &d19::registers[convert<u32>(ip_s)];
 
-    std::vector<ProgramLine> prog;
+    std::vector<d19::ProgramLine> prog;
     while (std::getline(input, line)) 
     {
         auto [ins, sa, sb, sc] = fixed_split<4>(line);
         i32 a = convert<i32>(sa);
         i32 b = convert<i32>(sb);
         i32 c = convert<i32>(sc);
-        prog.emplace_back(instruction_fn[ins], a, b, c);
+        prog.emplace_back(d19::instruction_fn[ins], a, b, c);
     }
 
     std::cout << factor_sum(false, ip, prog) << '\n';
     std::cout << factor_sum(true, ip, prog) << '\n';
 }
 
-void run(i32* ip, const std::vector<ProgramLine>& prog)
+void run(i32* ip, const std::vector<d19::ProgramLine>& prog)
 {
     while (*ip != 6) {
         prog[*ip].ins(prog[*ip].a, prog[*ip].b, prog[*ip].c);
@@ -96,20 +75,20 @@ void run(i32* ip, const std::vector<ProgramLine>& prog)
     }
 }
 
-u32 factor_sum(bool part2, i32* ip, const std::vector<ProgramLine>& prog)
+u32 factor_sum(bool part2, i32* ip, const std::vector<d19::ProgramLine>& prog)
 {
     for (i32 i = 0; i < 6; ++i) {
-        registers[i] = 0;
+        d19::registers[i] = 0;
     }
     if (part2) {
-        registers[0] = 1;
+        d19::registers[0] = 1;
     }
 
     run(ip, prog);
 
-    u32 ans = 1 + (registers[1] % 2 == 0);
-    for (u32 i = 3; i <= registers[1]; i += 2) {
-        if (registers[1] % i == 0) {
+    u32 ans = 1 + (d19::registers[1] % 2 == 0);
+    for (u32 i = 3; i <= d19::registers[1]; i += 2) {
+        if (d19::registers[1] % i == 0) {
             ans += i;
         }
     }
--- solutions/d21.cpp
+++ solutions/d21.cpp
@@ -1,10 +1,92 @@
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
+#include "utils.hpp"
 #include "solution.hpp"
+
+using itype = void(*)(i64, i64, i64);
+namespace d21
+{
+
+i64 registers[6]{0, 0, 0, 0, 0};
+
+std::unordered_map<std::string, itype> instruction_fn{
+    { "addr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] + registers[b]; } },
+    { "addi", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] + b; } },
+    { "mulr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] * registers[b]; } },
+    { "muli", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] * b; } },
+    { "banr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] & registers[b]; } },
+    { "bani", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] & b; } },
+    { "borr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] | registers[b]; } },
+    { "bori", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] | b; } },
+    { "setr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a]; } },
+    { "seti", [](i64 a, i64 b, i64 c) { registers[c] = a; } },
+    { "gtir", [](i64 a, i64 b, i64 c) { registers[c] = a > registers[b]; } },
+    { "gtri", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] > b; } },
+    { "gtrr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] > registers[b]; } },
+    { "eqir", [](i64 a, i64 b, i64 c) { registers[c] = a == registers[b]; } },
+    { "eqri", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] == b; } },
+    { "eqrr", [](i64 a, i64 b, i64 c) { registers[c] = registers[a] == registers[b]; } }
+};
+
+struct ProgramLine {
+    itype ins;
+    i64 a;
+    i64 b;
+    i64 c;
+};
+
+}
+
+i64 run(i64* ip, const std::vector<d21::ProgramLine>& prog, bool part2);
+void debug(i64* ip);
 
 template <>
 void
 solution<21>::solve(std::ifstream& input)
 {
-    std::cout << "Solution to day 21" << '\n';
+    std::string line;
+
+    std::getline(input, line);
+    auto [_, ip_s] = fixed_split<2>(line);
+    i64* ip = &d21::registers[convert<u32>(ip_s)];
+
+    std::vector<d21::ProgramLine> prog;
+    while (std::getline(input, line)) 
+    {
+        auto [ins, sa, sb, sc] = fixed_split<4>(line);
+        i64 a = convert<i64>(sa);
+        i64 b = convert<i64>(sb);
+        i64 c = convert<i64>(sc);
+        prog.emplace_back(d21::instruction_fn[ins], a, b, c);
+    }
+
+    d21::registers[0] = 0;
+    std::cout << run(ip, prog, false) << '\n';
+    std::cout << run(ip, prog, true) << '\n';
+}
+
+i64 run(i64* ip, const std::vector<d21::ProgramLine>& prog, bool part2)
+{
+    i64 prev = 0;
+    std::unordered_set<i64> seen;
+    while (*ip >= 0 and *ip < prog.size()) {
+        if (*ip + 2 == 30) {
+            if (not part2) {
+                return d21::registers[3];
+            }
+            if (seen.contains(d21::registers[3])) {
+                return prev;
+                std::cout << prev << '\n';
+                break;
+            }
+            seen.insert(d21::registers[3]);
+            prev = d21::registers[3];
+        }
+        prog[*ip].ins(prog[*ip].a, prog[*ip].b, prog[*ip].c);
+        *ip += 1;
+    }
+    return 0;
 }
