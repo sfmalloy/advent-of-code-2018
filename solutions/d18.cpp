d9311fbbccc8be6d4aa8f325f4b2bb4628832c96
Sean Malloy <techfan321@gmail.com>
2025-01-12 17:35:56 -0500

    days 18, 22

--- solutions/d18.cpp
+++ solutions/d18.cpp
@@ -1,10 +1,148 @@
 #include <iostream>
+#include <string>
 
 #include "solution.hpp"
+#include <unordered_map>
+
+bool open_to_tree(i32 r, i32 c, char* landscape);
+bool tree_to_lumberyard(i32 r, i32 c, char* landscape);
+bool lumberyard_to_open(i32 r, i32 c, char* landscape);
+char at(i32 r, i32 c, char* landscape);
+void print_grid(char* landscape);
+i32 transform_lanscape(char* landscape, u32 minutes);
+
+const constexpr i32 L = 50;
 
 template <>
 void
 solution<18>::solve(std::ifstream& input)
 {
-    std::cout << "Solution to day 18" << '\n';
+    char landscape1[L * L];
+    char landscape2[L * L];
+    std::string line;
+    for (i32 r = 0; std::getline(input, line); ++r) {
+        for (i32 c = 0; c < line.length(); ++c) {
+            landscape1[r*L + c] = line[c];
+            landscape2[r*L + c] = line[c];
+        }
+    }
+
+    std::cout << transform_lanscape(landscape1, 10) << '\n';
+    std::cout << transform_lanscape(landscape2, 1000000000) << '\n';
+}
+
+i32 transform_lanscape(char* landscape, u32 T) {
+    std::unordered_map<std::string, u32> seen;
+    for (u32 minute = 0; minute < T; ++minute) {
+        char transformed[L * L];
+        for (i32 r = 0; r < L; ++r) {
+            for (i32 c = 0; c < L; ++c) {
+                if (open_to_tree(r, c, landscape)) {
+                    transformed[r*L + c] = '|';
+                } else if (tree_to_lumberyard(r, c, landscape)) {
+                    transformed[r*L + c] = '#';
+                } else if (lumberyard_to_open(r, c, landscape)) {
+                    transformed[r*L + c] = '.';
+                } else {
+                    transformed[r*L + c] = landscape[r*L + c];
+                }
+            }
+        }
+
+        for (i32 i = 0; i < L * L; ++i) {
+            landscape[i] = transformed[i];
+        }
+        auto key = std::string{landscape};
+        if (!seen.contains(key)) {
+            seen[key] = minute;
+        } else {
+            u32 cycle_len = minute - seen[key];
+            return transform_lanscape(landscape, ((T - minute) % cycle_len) - 1);
+        }
+    }
+
+    i32 lumberyards = 0;
+    i32 trees = 0;
+    for (i32 r = 0; r < L; ++r) {
+        for (i32 c = 0; c < L; ++c) {
+            lumberyards += landscape[r*L + c] == '#';
+            trees += landscape[r*L + c] == '|';
+        }
+    }
+
+    return lumberyards * trees;
+}
+
+bool open_to_tree(i32 r, i32 c, char* landscape) {
+    if (at(r, c, landscape) != '.') {
+        return false;
+    }
+    i32 count = 0;
+    for (i32 rr = r - 1; rr <= r + 1; ++rr) {
+        for (i32 cc = c - 1; cc <= c + 1; ++cc) {
+            if (rr == r and cc == c) {
+                continue;
+            }
+            count += at(rr, cc, landscape) == '|';
+        }
+    }
+
+    return count >= 3;
+}
+
+bool tree_to_lumberyard(i32 r, i32 c, char* landscape) {
+    if (at(r, c, landscape) != '|') {
+        return false;
+    }
+    i32 count = 0;
+    for (i32 rr = r - 1; rr <= r + 1; ++rr) {
+        for (i32 cc = c - 1; cc <= c + 1; ++cc) {
+            if (rr == r and cc == c) {
+                continue;
+            }
+            count += at(rr, cc, landscape) == '#';
+        }
+    }
+
+    return count >= 3;
+}
+
+bool lumberyard_to_open(i32 r, i32 c, char* landscape) {
+    if (at(r, c, landscape) != '#') {
+        return false;
+    }
+    bool found_tree = false;
+    bool found_lumberyard = false;
+    for (i32 rr = r - 1; rr <= r + 1; ++rr) {
+        for (i32 cc = c - 1; cc <= c + 1; ++cc) {
+            if (rr == r and cc == c) {
+                continue;
+            }
+            char neighbor = at(rr, cc, landscape);
+            if (neighbor == '#') {
+                found_tree = true;
+            } else if (neighbor == '|') {
+                found_lumberyard = true;
+            }
+        }
+    }
+
+    return not (found_tree and found_lumberyard);
+}
+
+char at(i32 r, i32 c, char* landscape) {
+    if (r < L and r >= 0 and c < L and c >= 0) {
+        return landscape[r*L + c];
+    }
+    return 0;
+}
+
+void print_grid(char* landscape) {
+    for (i32 r = 0; r < L; ++r) {
+        for (i32 c = 0; c < L; ++c) {
+            std::cout << landscape[r*L + c];
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
 }
--- solutions/d22.cpp
+++ solutions/d22.cpp
@@ -1,10 +1,167 @@
-#include <iostream>
+#include <complex>
+#include <cstddef>
+#include <cstdlib>
+#include <functional>
+#include <limits>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 #include "solution.hpp"
+#include "utils.hpp"
+
+struct Node {
+    std::complex<i32> pos;
+    u32 item;
+
+    Node(std::complex<i32> pos, u32 item)
+    : pos{pos}
+    , item{item}
+    { }
+};
+
+constexpr const u32 NOTHING = 0;
+constexpr const u32 TORCH = 1;
+constexpr const u32 CLIMBING = 2;
+
+constexpr const std::complex U(-1, 0);
+constexpr const std::complex D(1, 0);
+constexpr const std::complex L(0, -1);
+constexpr const std::complex R(0, 1);
+
+template <>
+struct std::hash<std::complex<i32>>
+{
+    std::size_t operator()(const std::complex<i32>& p) const {
+        return std::hash<i32>()(p.real()) ^ (std::hash<i32>()(p.imag()) << 1);
+    }
+};
+
+template <>
+struct std::hash<std::pair<std::complex<i32>, u32>>
+{
+    std::size_t operator()(const std::pair<std::complex<i32>, u32>& p) const {
+        return std::hash<std::complex<i32>>()(p.first) ^ (std::hash<u32>()(p.second) << 1);
+    }
+};
+
+template <>
+struct std::hash<Node>
+{
+    std::size_t operator()(const Node& n) const {
+        return std::hash<std::complex<i32>>()(n.pos) ^ (std::hash<u32>()(n.item) << 1);
+    }
+};
+
+bool operator==(const Node& a, const Node& b) {
+    return a.item == b.item and a.pos == b.pos;
+}
+
+u32 erosion(std::complex<i32> pos, std::complex<i32> target, u32 depth, std::unordered_map<std::complex<i32>, u32>& map) {
+    if (map.contains(pos)) {
+        return map[pos];
+    }
+
+    if (pos == std::complex<i32>{0, 0} || pos == target) {
+        map[pos] = 0;
+    } else if (pos.imag() == 0) {
+        map[pos] = (pos.real() * 48271 + depth) % 20183;
+    } else if (pos.real() == 0) {
+        map[pos] = (pos.imag() * 16807 + depth) % 20183;
+    } else {
+        u32 geo_index = erosion(pos + U, target, depth, map) * erosion(pos + L, target, depth, map);
+        map[pos] = (geo_index + depth) % 20183;
+    }
+    return map[pos];
+}
+
+bool has(u32 item, const u32* arr, u32 L) {
+    for (u32 i = 0; i < L; ++i) {
+        if (arr[i] == item) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 template <>
 void
 solution<22>::solve(std::ifstream& input)
 {
-    std::cout << "Solution to day 22" << '\n';
+    auto depth = convert<u32>(split(readline(input))[1]);
+    auto [tx, ty] = type_split<i32, 2>(split(readline(input))[1], ",");
+    std::unordered_map<std::complex<i32>, u32> erosion_map;
+    std::unordered_map<std::complex<i32>, u32> cave;
+
+    std::complex<i32> target{ty, tx};
+    i32 p1 = 0;
+    for (i32 y = 0; y <= ty; ++y) {
+        for (i32 x = 0; x <= tx; ++x) {
+            auto pos = std::complex<i32>{y, x};
+            auto e = erosion(pos, target, depth, erosion_map) % 3;
+            p1 += e;
+            cave[pos] = e;
+        }
+    }
+    std::cout << p1 << '\n';
+
+    constexpr const u32 region_item[3][2] = {
+        {CLIMBING, TORCH}, // rocky
+        {CLIMBING, NOTHING}, // wet
+        {TORCH, NOTHING} // narrow
+    };
+
+    Node src(std::complex<i32>{0, 0}, TORCH);
+    std::unordered_map<Node, u32> dists;
+    dists[src] = 0;
+
+    // this is really really really slow. TODO: make a heap with dist-pointers instead
+    std::unordered_set<Node> q;
+    q.insert(src);
+    while (!q.empty()) {
+        auto best = std::numeric_limits<i32>::max();
+        auto curr = src;
+        for (auto& n : q) {
+            if (dists[n] < best) {
+                curr = n;
+                best = dists[n];
+            }
+        }
+        q.erase(q.find(curr));
+
+        if (curr.pos == target) {
+            if (curr.item != TORCH) {
+                continue;
+            }
+            std::cout << dists[curr] << '\n';
+            break;
+        }
+
+        for (const auto& dir : {U, L, D, R}) {
+            auto next = curr.pos + dir;
+
+            if (next.real() >= 0 and next.imag() >= 0) {
+                if (!cave.contains(next)) {
+                    cave[next] = erosion(next, target, depth, erosion_map) % 3;
+                }
+                for (u32 item : region_item[cave[next]]) {
+                    if (!has(item, region_item[cave[curr.pos]], 2)) {
+                        continue;
+                    }
+                    auto n = Node(next, item);
+                    if (!dists.contains(n)) {
+                        dists[n] = std::numeric_limits<i32>::max();
+                    }
+                    auto dist = dists[curr] + (item == curr.item ? 1 : 8);
+                    if (dist < dists[n]) {
+                        dists[n] = dist;
+                        q.insert(n);
+                    }
+                }
+            }
+        }
+    }
 }
